f9bb2c34ea2d7f8ffad981f1ecdcbf146833561f
arun01-ux <uarun1714@gmail.com>
2026-09-02 14:51:27 +0530

    Update abc..py

--- abc..py
+++ abc..py
@@ -1,6 +1,5 @@
 firstname = input("Enter your first name")
 lastname = input("Enter your last name")
 city = input("Enter your city")
-
 print("User first name and last name ", firstname + " " + lastname)
 print("User city", city)
